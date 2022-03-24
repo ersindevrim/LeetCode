62db7bfcb4e5bf666a3047ee41b8dae2ef6c1164
Ersin Devrim <ersin.devrim@outlook.com>
2022-03-24 10:51:26 +0300

    TwoSum

---
+++ Arrays/TwoSum.go
@@ -0,0 +1,16 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nums
+}
+
+func main() {
+	twoSum([]int{2, 7, 11, 15}, 9)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ersindevrim/leetcode
+
+go 1.17
