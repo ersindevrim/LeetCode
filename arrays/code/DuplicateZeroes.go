dac1cb7a0c43c6ef64044f1c5bd8e8eb30c73a06
Ersin Devrim <ersin.devrim@outlook.com>
2022-03-25 18:58:17 +0300

    *

---
+++ arrays/code/DuplicateZeroes.go
@@ -0,0 +1,20 @@
+package arrays
+
+func DuplicateZeros(arr []int) []int {
+
+	returnArray := make([]int, len(arr))
+	howManyZeroesAdded := 0
+
+	for i := 0; i < len(returnArray)-1; i++ {
+		returnArray[i+howManyZeroesAdded] = arr[i]
+		if arr[i] == 0 {
+			howManyZeroesAdded++
+			returnArray[i+howManyZeroesAdded] = 0
+		}
+		if i+howManyZeroesAdded == len(returnArray)-1 {
+			break
+		}
+	}
+
+	return returnArray
+}
---
+++ arrays/tests/DuplicateZeroes_test.go
@@ -0,0 +1,18 @@
+package arrays
+
+import (
+	"testing"
+
+	arrays "github.com/ersindevrim/leetcode/arrays/code"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDuplicateZeros(t *testing.T) {
+	t.Run("This should return correct value 2", func(t *testing.T) {
+		got := arrays.DuplicateZeros([]int{1, 0, 2, 3, 0, 4, 5, 0})
+		want := []int{1, 0, 0, 2, 3, 0, 0, 4}
+
+		assert.Equal(t, got, want)
+	})
+
+}
