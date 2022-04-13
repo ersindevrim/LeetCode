d7d734f67a0f945494ec783bee026feef998b67f
Ersin <ersin.devrim@outlook.com>
2022-04-13 14:34:07 +0300

    tests updated. Makefile added

--- arrays/tests/MaxConsecutiveOnes_test.go
+++ arrays/tests/MaxConsecutiveOnes_test.go
@@ -17,7 +17,7 @@ func TestBinnarySearch(t *testing.T) {
 
 	t.Run("This should return correct value 6", func(t *testing.T) {
 		got := arrays.FindMaxConsecutiveOnes([]int{1, 1, 1, 1, 1, 1, 0, 1, 1, 1})
-		want := 3
+		want := 6
 
 		assert.Equal(t, got, want)
 	})
